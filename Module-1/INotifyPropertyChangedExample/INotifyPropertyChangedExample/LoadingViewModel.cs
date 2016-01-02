9d22de71b0e5130e9d9fc05054b7db870cb47378
Andrew Ding <afding@gmail.com>
2016-01-02 11:56:40 -0600

    INotifyPropertyChangedExample Complete
    Implemented the INotifyPropertyChanged interface and used the class's
    properties in a data-binding scenario.

---
+++ Module-1/INotifyPropertyChangedExample/INotifyPropertyChangedExample/LoadingViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace INotifyPropertyChangedExample
+{
+    public class LoadingViewModel : INotifyPropertyChanged
+    {
+        private bool _showLoadingIndicator = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool ShowLoadingIndicator
+        {
+            get { return this._showLoadingIndicator; }
+            set
+            {
+                if (value != this._showLoadingIndicator)
+                {
+                    this._showLoadingIndicator = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        public LoadingViewModel()
+        {
+            this.ShowLoadingIndicator = true;
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string propName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
