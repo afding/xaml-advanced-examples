038a03bd27c729abd8f53e2a1cc3b0cff012cf56
Andrew Ding <afding@gmail.com>
2016-01-02 00:38:57 -0600

    Completed CustomEventExample
    Added an example where we implement a custom event.

---
+++ Module-4/CustomEventExample/CustomEventExample/ClickCircle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace CustomEventExample
+{
+    public sealed class ClickCircle : Control
+    {
+        public event EventHandler<MessageEventArgs> Click;
+
+        private void Circle_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (Click != null)
+            {
+                var device = e.PointerDeviceType.ToString();
+                var eventArgs = new MessageEventArgs($"This control was clicked by: {device}");
+                Click(sender, eventArgs);
+            }
+        }
+
+        public ClickCircle()
+        {
+            this.DefaultStyleKey = typeof(ClickCircle);
+            this.Tapped += Circle_Tapped;
+        }
+    }
+}
---
+++ Module-4/CustomEventExample/CustomEventExample/MessageEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEventExample
+{
+    public sealed class MessageEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+
+        public MessageEventArgs(string message)
+        {
+            this.Message = message;
+        }
+
+    }
+}
