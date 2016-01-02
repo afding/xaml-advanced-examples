5c133e466493dc661fe73f5a30147513fd539885
Andrew Ding <afding@gmail.com>
2016-01-02 10:55:41 -0600

    Completed DependencyPropertyExample
    Implemented a DependencyProperty in MainPage.xaml and data-bound to it
    in XAML.

---
+++ Module-1/DependencyPropertyExample/DependencyPropertyExample/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DependencyPropertyExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public Boolean IsLoading
+        {
+            get { return (Boolean)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsLoading.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsLoadingProperty =
+            DependencyProperty.Register("IsLoading", typeof(Boolean), typeof(MainPage), new PropertyMetadata(false));
+
+
+        async void LoadData()
+        {
+            this.IsLoading = true;
+            await Task.Delay(5000);
+            this.IsLoading = false;
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadData();
+        }
+    }
+}
