51433f6735ff51254bbb52e914707a446bdac16d
Andrew Ding <afding@gmail.com>
2016-01-02 15:05:04 -0600

    TemplateForListItemsExample Completed
    Created a DataTemplate that alters the rendering of each item in a list
    control in XAML

---
+++ Module-2/TemplateForListItemsExample/TemplateForListItemsExample/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace TemplateForListItemsExample.Models
+{
+    public class Item
+    {
+        public string Title { get; set; }
+
+        public Color SymbolColor { get; set; }
+    }
+}
